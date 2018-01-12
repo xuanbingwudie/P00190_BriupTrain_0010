b58d0c8027d64fd7a86299c94f3faaa7dae53e68
Kevin <2261060260@qq.com>
2018-01-12 11:32:23 +0800

    H

--- M00010_JdbcDemo_0010/src/main/java/com/briup/train/jdbc/BasicOperation.java
+++ M00010_JdbcDemo_0010/src/main/java/com/briup/train/jdbc/BasicOperation.java
@@ -14,30 +14,43 @@ public class BasicOperation{
         // 1、加载驱动类/注册驱动类
         String driver="com.mysql.jdbc.Driver";
         Class.forName(driver);
+
         // 2、获取连接对象
-        String url="jdbc:mysql://1.0.0.50:5718/briup";
+        String url="jdbc:mysql://127.0.0.1:5096/briup";
         String user="root";
         String password="root";
-        Connection connection=
-            DriverManager.getConnection(
-                url,user,password);
+        Connection connection=DriverManager.getConnection(url,user,password);
+
         // 3、获取Statement对象
-        Statement statement=
-            connection.createStatement();
+        Statement statement=connection.createStatement();
+
         // 4、执行SQL
-        String sql="select * from briup.s_emp";
-        ResultSet resultSet=
-            statement.executeQuery(sql);
+        String sql="select last_name as ln,salary"+
+            " from briup.s_emp limit 5";
+        ResultSet resultSet=statement.executeQuery(sql);
+
         // 5、如果执行的是select语句，一般会有结果集
         //    处理结果集
-
         while(resultSet.next()){
+            // 通过列名获取结果集
             String last_name=
-                resultSet.getString("last_name");
-            int salary=resultSet.getInt("salary");
+                resultSet.getString("ln");
+            int salary=
+                resultSet.getInt("salary");
             System.out.println(last_name+"::"+salary);
+
+            // 通过列的编号获取结果集
+            //String lname=resultSet.getString(1);
+            //int slry=resultSet.getInt(2);
+            //System.out.println(lname+"::"+slry);
+
+            // 通过列或者字段的别名来获取数据
+
         }
+
         // 6、关闭相关资源(I/O)
+        resultSet.close();
+        statement.close();
         connection.close();
     }
 }
